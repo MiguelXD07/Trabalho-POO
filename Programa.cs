93ac03abd50e5cf8712c61c557c6eb700bc08e2b
MiguelXD07 <miguelxd006@gmail.com>
2025-01-10 14:25:16 +0000

    Update Programa.cs

--- Programa.cs
+++ Programa.cs
@@ -1,10 +1,20 @@
+using System.Globalization;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Xml.Serialization;
+using System.Text.Json;
+
 Console.WriteLine("Bem-vindo ao Hospital ->Arruinado<-");
 
 CatalagoHospital hospital = new CatalagoHospital();
+
 int opcao;
 string nome;
 
+//Para ler para o Json        Esta a ler para o meu visual vcs têm de mudar o vosso path
+string filepath = @"C:\Users\migue\Desktop\Faculdade\P.O.O\TrabalhoDeGrupo\TrabalhoDeGrupo\Catalago.json";
 
+//Staff ja implementada
 hospital.AdicionarStaff(new Hospital("Miguel", "Medico", "Sirurgiao"));
 hospital.AdicionarStaff(new Hospital("Fernando", "Medico", "Neurosirurgiao"));
 hospital.AdicionarStaff(new Hospital("Ana", "Medica", "Medica de Familia"));
@@ -13,40 +23,73 @@ hospital.AdicionarStaff(new Hospital("Rodrigo", "Enfermeiro", "Assitente"));
 hospital.AdicionarStaff(new Hospital("Daniel", "Funcionario", "Limpezas"));
 hospital.AdicionarStaff(new Hospital("Margarida", "Secretaria", "Secetariado"));
 
+//Consultas ja implementadas
+hospital.AdicionarConsulta(new ConsultasPacientes(01, "Miguel", 01));
+hospital.AdicionarConsulta(new ConsultasPacientes(07, "Mario", 02));
+hospital.AdicionarConsulta(new ConsultasPacientes(10, "Susana", 03));
+
+
 do
 {
     Console.WriteLine("Escolha uma das opções:");
-    Console.WriteLine("1-Ver Staff\n2-Adicionar Staff\n3-Despedir Staff\n4-Pesquisar Staff\n0-Sair");
+    Console.WriteLine("1-Ver Staff\n2-Adicionar Staff\n3-Despedir Staff\n4-Pesquisar Staff\n5-Lista de Consultas\n6-Adicionar Consulta\n7-Grava Json\n8-Lê Json\n0-Sair");
     opcao = int.Parse(Console.ReadLine());
 
     switch (opcao)
     {
         case 1:
             hospital.ListaStaff();
+
             break;
 
         case 2:
             AdicionarStaff();
+
             break;
 
         case 3:
             RemoverStaff();
+
             break;
 
         case 4:
             PesquisarStaffPorNome();
+
             break;
+
+        case 5:
+            hospital.ListaConsultas();
+
+            break;
+
+        case 6:
+            AdicionarConsulta();
+
+            break;
+
+        case 7:
+            GravaCatalogo(hospital);
+
+            break;
+
+        case 8:
+            hospital = LerCatalago();
+            hospital.ListaStaff();
+            hospital.ListaConsultas();
+
+            break;
+
         case 0:
             Console.WriteLine("Adeus");
             break;
     }
 } while (opcao != 0);
 
-
+//Funções para Staff
 void AdicionarStaff()
 {
     Console.WriteLine("Escreva o Nome:");
-    string staff = Console.ReadLine();
+    string nome = Console.ReadLine();
 
     Console.WriteLine("Escreva a Categoria (Medico, Enfermeiro, Secretaria, Funcionario):");
     string categorias = Console.ReadLine();
@@ -54,7 +97,7 @@ void AdicionarStaff()
     Console.WriteLine("Escreva a Especialidade:");
     string especialidades = Console.ReadLine();
 
-    Hospital hospital1 = new Hospital(staff, categorias, especialidades);
+    Hospital hospital1 = new Hospital(nome, categorias, especialidades);
     hospital.AdicionarStaff(hospital1);
 }
 
@@ -77,4 +120,65 @@ void PesquisarStaffPorNome()
     nome = Console.ReadLine();
 
     hospital.PesquisarStaffPorNome(nome);
+}
+
+
+//Funções para Consultas
+void AdicionarConsulta()
+{
+    if (hospital.numeroConsultas < 10) //Verifica se há consultas disponíveis
+    {
+        hospital.ListaConsultasDisponiveis(); //Mostra as consultas disponíveis
+        Console.WriteLine("Número da consulta (2 dígitos):");
+        int consulta = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Escreva o Nome do Paciente:");
+        string paciente = Console.ReadLine();
+
+        hospital.ListaCamasDisponiveis(); //Mostra as camas disponíveis
+        Console.WriteLine("Insira o número da cama (2 dígitos):");
+        int cama = int.Parse(Console.ReadLine());
+
+        if (hospital.camasDisponiveis.Contains(cama)) //Verifica se a cama está disponível
+        {
+            if (hospital.consultasDisponiveis.Contains(consulta)) //Verifica se a consulta está disponível
+            {
+                ConsultasPacientes consultas1 = new ConsultasPacientes(consulta, paciente, cama);
+                hospital.AdicionarConsulta(consultas1);
+            }
+            else
+            {
+                Console.WriteLine("Número da consulta já utilizado. Por favor, escolha outra consulta.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Cama indisponível. Por favor, escolha outra cama.");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Lista de consultas cheio.");
+    }
+}
+
+
+
+//Ficheiro Json
+void GravaCatalogo(CatalagoHospital catalago)
+{
+    var option = new JsonSerializerOptions { WriteIndented = true };
+    string conteudo = JsonSerializer.Serialize(catalago, option);
+    File.WriteAllText(filepath, conteudo);
+    Console.WriteLine("Ficheiro Guardado com Êxito");
+    Console.WriteLine("");
+}
+
+CatalagoHospital LerCatalago()
+{
+
+
+    string conteudo = File.ReadAllText(filepath);
+    return JsonSerializer.Deserialize<CatalagoHospital>(conteudo);
+
 }
