e1fbd1ed14ab324672cff0baf7a2cd2fafd0a6b8
MiguelXD07 <miguelxd006@gmail.com>
2025-01-10 14:26:07 +0000

    Update Hospital.cs

--- Hospital.cs
+++ Hospital.cs
@@ -1,21 +1,47 @@
+using System.Text.Json.Serialization;
+
+//Para a Staff
 public class Hospital
 {
-    public string Staff;
+    public string Nome { get; set; }
+    public string Categorias { get; set; }
+    public string Especialidades { get; set; }
 
-    public string Categorias;
+    //Construtor padrão necessário para deserialização
+    public Hospital() { }
 
-    public string Especialidades;
-
-
-
-    public Hospital(string staff, string categorias, string especialidades)
+    public Hospital(string nome, string categorias, string especialidades)
     {
-        this.Staff = staff;
+        this.Nome = nome;
         this.Categorias = categorias;
         this.Especialidades = especialidades;
     }
+
     override public string ToString()
     {
-        return "Staff: " + Staff + ", Categoria: " + Categorias + ", Especialidade: " + Especialidades;
+        return "Staff: " + Nome + ", Categoria: " + Categorias + ", Especialidade: " + Especialidades;
+    }
+}
+
+//Para Consultas e Pacientes
+public class ConsultasPacientes
+{
+    public int Consulta { get; set; }
+    public string Paciente { get; set; }
+    public int Cama { get; set; }
+
+    //Construtor padrão necessário para deserialização
+    public ConsultasPacientes() { }
+
+    public ConsultasPacientes(int consulta, string paciente, int camas)
+    {
+        this.Consulta = consulta;
+        this.Paciente = paciente;
+        this.Cama = camas;
+    }
+
+    override public string ToString()
+    {
+        return "Consulta:  0" + Consulta + ", Paciente: " + Paciente + ", Cama: 0" + Cama;
     }
 }
